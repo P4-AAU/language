f8b3a53666c5fd9259f317a11fa896f5be22e24e
smilo1998 <sebastianjmilo@outlook.com>
2026-03-15 13:44:37 +0100

    ast beginning

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,5 +1,69 @@
+(* Abstract Syntax of While Language. *)
+
+(* Identifiers *)
+type location = Lexing.position * Lexing.position
+
+type ident =
+  { loc : location
+  ; id : string
+  }
+
+(* Unary Operators*)
+type unop =
+  | Uneg (* -e *)
+  | Unot (* not e *)
+
+(* Binary Operators*)
+type binop =
+  | Badd
+  | Bsub
+  | Bmul
+  | Bdiv
+  | Bmod
+  | Beq
+  | Bneq
+  | Blt (* less than *)
+  | Ble (* Less or equal*)
+  | Bgt (* Greater than*)
+  | Bge (* Greater than or equal*)
+  | Band
+  | Bor
+
+type constant =
+  | Cbool of bool
+  | Cint8 of int
+  | Cint16 of int
+  | Cint32 of int
+  | Cint64 of int
+  | Cuint8 of int
+  | Cuint16 of int
+  | Cuint32 of int
+  | Cuint64 of int
+  | Cstring of string
+  | Carray of constant
+
 type expr =
-  | Int of int
-  | Var of string
-  | Add of expr * expr
-  | Let of string * expr * expr
+  | Ecst of constant (* constant*)
+  | Eunop of unop * expr (* Unary operation*)
+  | Ebinop of binop * expr * expr
+  | Eident of ident (* Variable *)
+  | Earray of expr list
+  | Eindex of expr * expr (* a[i]*)
+  | Elen of expr
+
+type pattern =
+  | Pdefault (* default eller _ alt efter vi aftaler*)
+  | Pconst of constant
+  | Pvar of ident
+
+type stmt =
+  | Sif of expr * stmt * stmt (* if expr then stmt else stmt*)
+  | Sassign of ident * expr (* ident = expr *)
+  | Sblock of stmt list (* list of statements *)
+  | Sprint of expr list (* print a list of expressions*)
+  | Swhile of expr * stmt
+  | Sfor of stmt * expr * stmt * stmt (*assignment; condition; increment {body}*)
+  | Smatch of expr * (pattern * stmt) list (* match expression with a pattern and stmt*)
+  | Sindexset of expr * expr * expr
+
+type file = stmt
